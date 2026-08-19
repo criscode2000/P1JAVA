fa50c046063ecdd7bfc664fbd6cf974081204747
criscode2000 <crisccem2000@gmail.com>
2026-08-18 19:41:13 -0500

    App.java

---
+++ src/App.java
@@ -0,0 +1,15 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    int a = 10, b = 2;
+    System.out.println("La suma es: " + (a + b));
+    System.out.println("La resta es: " + (a - b));
+    System.out.println("La multiplicacion es: " + (a * b));
+    System.out.println("La division es: " + (a / b));
+    System.out.println("El modulo es: " + (a % b));
+    }
+}
+/*  App
+codigo
+18082026
+*/
---
+++ src/MiClase.java
@@ -0,0 +1,15 @@
+public class MiClase {
+private int edad = 25;
+private float altura = 1.93f;
+private char sexo = 'M'; // o 'F'
+private boolean Aprobado= false;
+private String nombre = "Cristhian";
+private String Saludo = "Hola, " + nombre;
+system.out.println(Saludo);
+system.out.println("Edad: " + edad);
+system.out.println("Altura: " + altura);
+system.out.println("Sexo: " + sexo);
+system.out.println("Aprobado: " + Aprobado);
+system.out.println("Nombre: " + nombre);
+system.out.println("Saludo: " + Saludo);
+}
